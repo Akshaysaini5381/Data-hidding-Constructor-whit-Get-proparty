3598497752e46e7210f8a03b4d7ff987aa245561
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-24 23:56:44 +0530

    Add project files.

---
+++ Data hidding Constructor whit Get proparty/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_hidding_Constructor_whit_Get_proparty
+{
+    class data
+    {
+        private float num;
+
+        public data(float num)
+        {
+            this.num = num;
+        }
+        public float number
+        {
+            get
+            {
+                return this.num;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            data obj = new data(20.20f);
+            float n = obj.number;
+            Console.WriteLine(n);
+           /* Console.WriteLine(obj.number);*/
+            Console.ReadLine();
+
+        }
+    }
+}
